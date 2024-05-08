7a14a5c6bf6147e5c7f8ad919b8dead797077144
aleksandrawozniak965 <aleksandra.wozniak965@gmail.com>
2024-05-08 19:09:51 +0200

    Changing Page Title

--- src/App v-1.js
+++ src/App v-1.js
@@ -351,6 +351,14 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     [selectedId]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `MOVIE | ${title}`;
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
