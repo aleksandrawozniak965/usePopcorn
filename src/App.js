bc3cbb36b3713b14e512db957ef2b53b9efe955b
aleksandrawozniak965 <aleksandra.wozniak965@gmail.com>
2024-05-11 20:55:49 +0200

    Watched movie list to localStorage

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ const KEY = "1251c7ea";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState("");
+
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   // WARNING SIDE EFFECT AT COMPONENT RENDER LOGIC - INFINITY LOOP
   // fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
@@ -31,6 +36,10 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(() => {
+    localStorage.setItem("watched", JSON.stringify(watched));
+  }, [watched]);
 
   useEffect(
     function () {
