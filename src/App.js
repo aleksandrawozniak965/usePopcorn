00394a437df6ecc5bbf400a4b8552cccb93e585c
aleksandrawozniak965 <aleksandra.wozniak965@gmail.com>
2024-05-12 17:57:16 +0200

    focus fn - press enter

--- src/App.js
+++ src/App.js
@@ -177,8 +177,21 @@ function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
   useEffect(() => {
-    inputEl.current.focus();
-  }, []);
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+
+    return () => {
+      document.addEventListener("keydown", callback);
+    };
+  }, [setQuery]);
 
   return (
     <input
